720c1b7cb5be26b86135be0b91a6c759296e0269
azat <azatmagdeev@gmail.com>
2021-05-31 03:19:45 +0300

    scss files oversize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    './app-menu.component.scss',
+  ]
 })
 export class AppComponent {
   title = 'need-for-drive';
