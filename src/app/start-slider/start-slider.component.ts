aee22c5c5679bd5de50e03a57e094e0819e6b3d4
azat <azatmagdeev@gmail.com>
2021-06-14 02:14:56 +0300

    refact start-slider.component

--- src/app/start-slider/start-slider.component.ts
+++ src/app/start-slider/start-slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {slides} from '../../constants';
 
 @Component({
@@ -8,72 +8,76 @@ import {slides} from '../../constants';
 })
 export class StartSliderComponent implements OnInit {
 
-  slides = slides;
-  activeSlideIndex: number | undefined;
-  slideWidth: number | undefined = 0;
-  private intervalId: number | undefined = undefined;
+  @ViewChild("slideEl")
+  slideEl?: ElementRef;
 
-  getActiveSlideIndex() {
-    this.activeSlideIndex = this.slides.findIndex(slide => slide.active)
-  }
-
-  getSlideWidth() {
-    // @ts-ignore
-    this.slideWidth = document.querySelector('.slider__slide').getBoundingClientRect().width
-  }
+  private intervalId?: number;
+  slides: {
+    descr: string;
+    img: string;
+    active: boolean;
+    title: string
+  }[];
 
   constructor() {
-    this.getActiveSlideIndex();
-    this.setInterval();
-  }
-
-  setInterval() {
-    this.intervalId = setInterval(()=>this.next(),7000)
+    this.slides = slides;
   }
 
   ngOnInit(): void {
+    this.setInterval();
   }
 
-  selectSlide($event: Event) {
+  ngOnDestroy(): void {
     clearInterval(this.intervalId)
-    this.slides.forEach(slide => slide.active = false);
-    // @ts-ignore
-    const slide = this.slides.find(slide => slide.title === $event.target.title)
-    // @ts-ignore
-    slide.active = true;
-    this.getSlideWidth();
-    this.getActiveSlideIndex();
-    this.setInterval();
+  }
+
+  getActiveSlideIndex() {
+    return this.slides.findIndex(slide => slide.active)
+  }
+
+  getSlideWidth() {
+    if (this.slideEl) return this.slideEl.nativeElement.clientWidth;
+  }
+
+  setInterval() {
+    this.intervalId = setInterval(() => {
+      this.setActiveSlide(this.getNextIndex());
+    }, 7000)
+  }
+
+  getNextIndex(){
+    const newIndex = this.getActiveSlideIndex() + 1;
+    return newIndex >= this.slides.length? 0 : newIndex;
+  }
+
+  selectSlide(title: string) {
+    const newIndex = this.slides.findIndex(slide => slide.title === title);
+    this.changeActiveSlideByUser(newIndex)
   }
 
   next() {
-    clearInterval(this.intervalId)
-    const index = this.activeSlideIndex;
-    // @ts-ignore
-    let newIndex = index + 1;
-    if (newIndex >= this.slides.length) newIndex = 0;
-    this.slides.forEach(slide => slide.active = false);
-    this.slides[newIndex].active = true;
-    this.getSlideWidth();
-    this.getActiveSlideIndex();
-    this.setInterval();
+    this.changeActiveSlideByUser(this.getNextIndex())
   }
 
   prev() {
-    clearInterval(this.intervalId)
-    const index = this.activeSlideIndex;
-    // @ts-ignore
-    let newIndex = index - 1;
+    let newIndex = this.getActiveSlideIndex() - 1;
     if (newIndex < 0) newIndex = this.slides.length - 1;
-    this.slides.forEach(slide => slide.active = false);
-    this.slides[newIndex].active = true;
-    this.getSlideWidth();
-    this.getActiveSlideIndex();
+    this.changeActiveSlideByUser(newIndex)
+  }
+
+  changeActiveSlideByUser(index: number) {
+    clearInterval(this.intervalId);
+    this.setActiveSlide(index);
     this.setInterval();
   }
 
+  setActiveSlide(index: number) {
+    this.slides.forEach(slide => slide.active = false);
+    this.slides[index].active = true;
+  }
+
   calculateRight() {
-    // @ts-ignore
-    return (this.activeSlideIndex * this.slideWidth) + 'px'
+    const right = this.getActiveSlideIndex() * this.getSlideWidth();
+    return (right ? right : 0) + 'px'
   }
 }
